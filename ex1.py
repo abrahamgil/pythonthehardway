392d67ae91a71bef5670809b409028ed36b10df5
Abraham <abrahamgil2@gmail.com>
2020-04-27 04:25:34 -0500

    This is the first exercise for Python The Hard Way

---
+++ ex1.py
@@ -0,0 +1,8 @@
+"""print("Hello World!")
+print("Hello Again")
+print("I like typthing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')"""
+print("This is my own line. I commented out the other lines")
