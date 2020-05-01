aaf950d3068d7b7e34b597e20d5e4d015e475f59
Abraham <abrahamgil2@gmail.com>
2020-05-01 04:38:25 -0500

    Worked on exercise 3 and completed study drills #1

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:") #This line of code display the string 'I will now count my chickens:'
+
+print("Hens ", 25 + 30 / 6) #This line prints the string of Hens and then does the math. 1st it divides 30 by 6 and then adds it to 25 which displays 30
+print("Roosters ", 100 - 25 * 3 % 4) #This line displays the string Roosters and then does the math. It first multiplies 25 * 3 which gives 75 then does the Modulus of 75 by 4 whic
+# results in 3 and finally it subtracts 100 by 3 which displays 97
+
+print("Now I will count the eggs: ") #This line displays Now I will count the eggs: 
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #This line does the math. First it does the Mod which is 0 then it divides 1/4 which is .25 and finally it adds and subtracts which is 6.75
+
+print("Is it true that 3 + 2 < 5 - 7? ") #This line prints the string Is it true that 3 + 2 < 5 - 7?
+
+print(3 + 2 < 5 - 7) #This line compares the result of 5 and -2 which displays False
+
+print("What is 3 + 2? ", 3 + 2) #This line displays the string and the result of 3 + 2
+print("What is 5 - 7? ", 5 - 7) #This line displays the string and the result of 5 - 7
+
+print("Oh, that's why it's False. ") #This line displays the string
+
+print("How about some more?") #This line displays the string
+
+print("Is it greater? ", 5 > -2) #This line displays the string and does the comparison of 5 > -2 which displays TRUE
+print("Is it greater or equal?", 5 >= -2) #This line displays the string and does the comparision to see if 5 is greater or equal to -2 which displays TRUE
+print("Is it less or equal? ", 5 <= -2) #This line displays the string and does the comparison to see if 5 less than or equal to -2 which displays FALSE
